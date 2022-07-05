ed01e5852653e159e5096e4d67a91ea32f38bf94
SelenaInt <selen15@yandex.ru>
2022-07-05 22:28:02 +0300

    Home work_task29

---
+++ HW_task29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int size = 8;
+int [] seriesNumbers = myArray(size);
+Console.WriteLine($"[{String.Join("; ", seriesNumbers )}]");
+
+int[] myArray(int size)
+{
+    int[] resultArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = new Random().Next(size);
+    }
+    return resultArray;
+}
+
+
+
